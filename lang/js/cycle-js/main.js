52851698f2303a3319a0133ce58da30f7cc9356f
ryym <ryym.64@gmail.com>
2015-10-21 22:26:47 +0900

    Cycle: Add a counter example

--- lang/js/cycle-js/main.js
+++ lang/js/cycle-js/main.js
@@ -5,7 +5,9 @@
 import checkboxToggle from './src/checkbox-toggle';
 import timer from './src/timer';
 import randomUser from './src/random-user';
+import counter from './src/counter';
 
 checkboxToggle();
 timer();
 randomUser();
+counter();
