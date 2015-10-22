cc6185030a1498904f29e7c475acbfa1698dd0f4
ryym <ryym.64@gmail.com>
2015-10-22 11:28:30 +0900

    Cycle: Separate functionalities as intent, model and view

--- lang/js/cycle-js/src/bmi-calculator.js
+++ lang/js/cycle-js/src/bmi-calculator.js
@@ -2,58 +2,84 @@ import Rx from 'rx';
 import Cycle from '@cycle/core';
 import {h, makeDOMDriver} from '@cycle/dom';
 
+function calculateBMI(weight, height) {
+  let heightMeters = height * 0.01;
+  return Math.round( weight / (heightMeters * heightMeters) );
+}
 
-function main({ DOM }) {
-  function calcBmi(weight, height) {
-    let heightMeters = height * 0.01;
-    return Math.round( weight / (heightMeters * heightMeters) );
-  }
+/**
+ * Interpret DOM events as user's intended actions.
+ * @param  DOM DOM Driver responses
+ * @return Action observables
+ */
+function intent(DOM) {
+  return {
+    changeWeight$: DOM.select('#weight').events('input')
+      .map(ev => ev.target.value),
 
-  let changeWeight$ = DOM.select('#weight').events('input')
-    .map(ev => ev.target.value);
+    changeHeight$: DOM.select('#height').events('input')
+      .map(ev => ev.target.value)
+  };
+}
 
-  let changeHeight$ = DOM.select('#height').events('input')
-    .map(ev => ev.target.value);
-
+/**
+ * Manage state.
+ * @param  Action observables
+ * @return `state$` observable
+ */
+function model(actions) {
   // `combineLatest` has 'AND' semantics on the other hand `merge` has 'OR' semantics.
-  let state$ = Rx.Observable.combineLatest(
-    changeWeight$.startWith(70),
-    changeHeight$.startWith(170),
+  return Rx.Observable.combineLatest(
+    actions.changeWeight$.startWith(70),
+    actions.changeHeight$.startWith(170),
     (weight, height) => {
-      let bmi = calcBmi(weight, height);
-      return { weight, height, bmi };
+      return { weight, height, bmi: calculateBMI(weight, height) };
     }
   );
+}
 
-  let vtree$ = state$.map(({ weight, height, bmi }) => {
+/**
+ * Visually represent state from the Model.
+ * @param  `state$` observable
+ * @return Observable of VTree as the DOM Driver request
+ */
+function view(state$) {
+  return state$.map(({ weight, height, bmi }) => {
     return h('div', [
-      h('h1', 'BMI Calculator'),
-
-      h('div', [
-        `Weight ${weight}kg`,
-        h('input#weight', {
-          type  : 'range',
-          value : weight,
-          min   : 40,
-          max   : 140
-        })
-      ]),
-
-      h('div', [
-        `Height ${height}cm`,
-        h('input#height', {
-          type  : 'range',
-          value : height,
-          min   : 140,
-          max   : 210
-        })
-      ]),
-
-      h('h2', 'BMI is ' + bmi)
+        h('h1', 'BMI Calculator'),
+        renderWeightSlider(weight),
+        renderHeightSlider(height),
+        h('h2', 'BMI is ' + bmi)
     ]);
   });
+}
 
-  return { DOM: vtree$ };
+function renderWeightSlider(weight) {
+  return h('div', [
+      `Weight ${weight}kg`,
+      h('input#weight', {
+        type  : 'range',
+        value : weight,
+        min   : 40,
+        max   : 140
+      })
+  ]);
+}
+
+function renderHeightSlider(height) {
+  return h('div', [
+      `Height ${height}cm`,
+      h('input#height', {
+        type  : 'range',
+        value : height,
+        min   : 140,
+        max   : 210
+      })
+  ]);
+}
+
+function main({ DOM }) {
+  return { DOM: view(model( intent(DOM) )) };
 }
 
 export default function() {
