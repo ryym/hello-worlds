c0178f35db7748e7d2cb7f68215e7f8c71304a59
ryym <ryym.64@gmail.com>
2016-03-27 13:34:46 +0900

    Add spring-boot-demo

---
+++ lang/java/spring-boot-demo/src/main/java/com/example/HelloController.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "hello")
+	public String hello(Model model) {
+		model.addAttribute("name", "Bizreach");
+		return "hello";
+	}
+}
+
