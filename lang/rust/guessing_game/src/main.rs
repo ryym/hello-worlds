f59dbdbc0823b7271948825d307e682d3e5f85d9
ryym <ryym.64@gmail.com>
2016-07-18 16:02:32 +0900

    Rust: Generate random number

--- lang/rust/guessing_game/src/main.rs
+++ lang/rust/guessing_game/src/main.rs
@@ -1,7 +1,15 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    // 'use rand::Rng' (trait) makes gen_range method available.
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("secret number: {}", secret_number);
 
     println!("Please input your guess.");
 
