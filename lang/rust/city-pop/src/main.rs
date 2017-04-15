e0099214ea65f852a9078eb5f5758188e103ec9e
ryym <ryym.64@gmail.com>
2017-04-15 10:08:01 +0900

    Rust: Error handling example (try!, Result, Box<Error>)

--- lang/rust/city-pop/src/main.rs
+++ lang/rust/city-pop/src/main.rs
@@ -6,6 +6,7 @@ use getopts::Options;
 use std::env;
 use std::fs::File;
 use std::path::Path;
+use std::error::Error;
 
 // This struct represents the data in each row of the CSV file.
 #[derive(Debug, RustcDecodable)]
@@ -32,13 +33,13 @@ fn print_usage(program: &str, opts: Options) {
              opts.usage(&format!("Usage: {} [options] <data-path> <city>", program)))
 }
 
-fn search<P: AsRef<Path>>(file_path: P, city: &str) -> Vec<PopulationCount> {
+fn search<P: AsRef<Path>>(file_path: P, city: &str) -> Result<Vec<PopulationCount>, Box<Error>> {
     let mut found = vec![];
-    let file = File::open(file_path).unwrap();
+    let file = try!(File::open(file_path));
     let mut rdr = csv::Reader::from_reader(file);
 
     for row in rdr.decode::<Row>() {
-        let row = row.unwrap();
+        let row = try!(row);
         match row.population {
             None => {} // Skip it.
             Some(count) => {
@@ -52,7 +53,12 @@ fn search<P: AsRef<Path>>(file_path: P, city: &str) -> Vec<PopulationCount> {
             }
         }
     }
-    found
+
+    if found.is_empty() {
+        Err(From::from("No matching cities with a population were found."))
+    } else {
+        Ok(found)
+    }
 }
 
 fn main() {
@@ -75,7 +81,12 @@ fn main() {
     let data_path = &matches.free[0]; // free: Vector<String>
     let city: &str = &matches.free[1];
 
-    for pop in search(data_path, city) {
-        println!("{}, {}: {:?}", pop.city, pop.country, pop.count)
+    match search(data_path, city) {
+        Ok(pops) => {
+            for pop in pops {
+                println!("{}, {}: {:?}", pop.city, pop.country, pop.count)
+            }
+        }
+        Err(err) => println!("{}", err),
     }
 }
