9c7f8f42fbf74b0992df57aa33c568cef28b6978
ryym <ryym.64@gmail.com>
2017-10-23 23:47:04 +0900

    Haskell(second-language): Use NonEmpty to ensure list is not empty

--- lang/haskell/blackjack/src/Blackjack.hs
+++ lang/haskell/blackjack/src/Blackjack.hs
@@ -9,26 +9,33 @@ module Blackjack
     , sumHand
     ) where
 
+import qualified Data.List.NonEmpty as NonEmpty
+import Data.List.NonEmpty (NonEmpty((:|)))
+import Data.Semigroup (sconcat)
+
 data Card = A | N Int | J | Q | K deriving (Eq, Show)
 
 sumHand :: [Card] -> Int
 sumHand cards =
     let possiblePoints = map toPoint cards
-        scoreCands = foldl plusEach [0] possiblePoints
-        noBust = filter (<= 21) scoreCands
+        scoreCands = foldl plusEach (0 :| []) possiblePoints
+        noBust = NonEmpty.filter (<= 21) scoreCands
     in
         if null noBust
-            then head scoreCands
-            else maximum noBust --foldl max 0 noBust
+            then NonEmpty.head scoreCands
+            else maximum noBust
 
-plusEach :: [Int] -> [Int] -> [Int]
-plusEach xs = concatMap (\y -> map (+ y) xs)
+plusEach :: NonEmpty Int -> NonEmpty Int -> NonEmpty Int
+plusEach xs = concatNonEmpty (\y -> NonEmpty.map (+ y) xs)
 
-toPoint :: Card -> [Int]
+concatNonEmpty :: (a -> NonEmpty b) -> NonEmpty a -> NonEmpty b
+concatNonEmpty f as = sconcat $ NonEmpty.map f as
+
+toPoint :: Card -> NonEmpty Int
 toPoint c = case c of
-    N n -> [n]
-    A -> [1, 11]
-    _ -> [10]
+    N n -> n :| []
+    A -> 1 :| [11]
+    _ -> 10 :| []
 
 suit, heartSuit, diaSuit, cloverSuit, spadeSuit :: [Card]
 suit = [A] ++ map N [2..10] ++ [J, Q, K]
