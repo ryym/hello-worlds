a16c1898b00616f75c829f7156dab4faa3acba59
ryym <ryym.64@gmail.com>
2017-10-26 16:17:44 +0900

    Haksell(mtsbs): Use State to hold intermediate state

--- lang/haskell/mtsbs/src/Transformers.hs
+++ lang/haskell/mtsbs/src/Transformers.hs
@@ -146,3 +146,45 @@ eval3 (App e1 e2) = do val1 <- eval3 e1
                             FunVal env' n body
                                 -> local (const (Map.insert n val2 env')) (eval3 body)
                             _   -> throwError "type error in application"
+
+-- Stateモナドで計算中の状態を保持する
+type Eval4 a = ReaderT Env (ExceptT String (StateT Integer Identity)) a
+runEval4 :: Env -> Integer -> Eval4 a -> (Either String a, Integer)
+runEval4 env st ev = runIdentity $ runStateT (runExceptT $ runReaderT ev env) st
+-- (ExceptT と StateT の包含関係が逆だと、戻り値は`Either (String (a, Integer))`になる)
+
+tick :: (Num s, MonadState s m) => m ()
+tick = do
+    st <- get
+    put (st + 1)
+
+eval4 :: Exp -> Eval4 Value
+eval4 (Lit i) = do
+    tick
+    return $ IntVal i
+eval4 (Var n) = do
+    tick
+    env <- ask
+    case Map.lookup n env of
+        Nothing -> fail ("undefined variable: " ++ n)
+        Just val -> return val
+eval4 (Plus e1 e2) = do
+    tick
+    e1' <- eval4 e1
+    e2' <- eval4 e2
+    case (e1', e2') of
+        (IntVal i1, IntVal i2)
+            -> return $ IntVal (i1 + i2)
+        _   -> throwError "type error in addition"
+eval4 (Abs n e) = do
+    tick
+    env <- ask
+    return $ FunVal env n e
+eval4 (App e1 e2) = do
+    tick
+    val1 <- eval4 e1
+    val2 <- eval4 e2
+    case val1 of
+        FunVal env' n body
+            -> local (const (Map.insert n val2 env')) (eval4 body)
+        _   -> throwError "type error in application"
