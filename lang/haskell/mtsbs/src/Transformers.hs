f85b9dac7489e6c040562f194ff84f6232db7539
ryym <ryym.64@gmail.com>
2017-10-26 15:49:34 +0900

    Haksell(mtsbs): Use Reader to hide environments

--- lang/haskell/mtsbs/src/Transformers.hs
+++ lang/haskell/mtsbs/src/Transformers.hs
@@ -118,3 +118,31 @@ eval2 env (App e1 e2)  = do val1 <- eval2 env e1
                                 FunVal env' n body
                                     -> eval2 (Map.insert n val2 env') body
                                 _   -> throwError "type error"
+
+-- https://wiki.haskell.org/All_About_Monads
+
+type Eval3 a = ReaderT Env (ExceptT String Identity) a
+runEval3 :: Env -> Eval3 a -> Either String a
+runEval3 env ev = runIdentity $ runExceptT $ runReaderT ev env
+
+-- Readerモナドを使う事で、必要な場合にだけ env にアクセスする形にできる。
+eval3 :: Exp -> Eval3 Value
+eval3 (Lit i) = return $ IntVal i
+eval3 (Var n) = do env <- ask
+                   case Map.lookup n env of
+                       Nothing -> fail ("undefined variable: " ++ n)
+                       Just val -> return val
+eval3 (Plus e1 e2) = do e1' <- eval3 e1
+                        e2' <- eval3 e2
+                        case (e1', e2') of
+                            (IntVal i1, IntVal i2)
+                                -> return $ IntVal (i1 + i2)
+                            _   -> throwError "type error in addition"
+eval3 (Abs n e) = do env <- ask
+                     return $ FunVal env n e
+eval3 (App e1 e2) = do val1 <- eval3 e1
+                       val2 <- eval3 e2
+                       case val1 of
+                            FunVal env' n body
+                                -> local (const (Map.insert n val2 env')) (eval3 body)
+                            _   -> throwError "type error in application"
